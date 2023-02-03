dd0fd9e6d7e64fa25048c8a8372e199a360ed082
barath121 <nadar.barath18@siesgst.ac.in>
2023-02-04 03:50:09 +0530

    Mail Dynamic Flow

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,13 +1,41 @@
-let mails = 
-[
-    {
-        "from" : "Barath",
-        "heading" : "Long Time No See",
-        "message" : "Hack him now he is using",
-        "date" : "Mar 5",
-        "image" : "../images/img_avatar.png",
-        "visible" : true
-    }
-]
+let emails;
+$.getJSON("../data/mail.json", function (data) {
+    $('#mail').hide();
+    emails = data;
+    data.forEach(email => {
+        let emailElement = `<li class="message" id="${email.uid}" onclick=openMail(this.id)>
+    <div class="mail-grid">
+        <img src="${email.image}" alt="Avatar">
+        <div>
+            <div class="date">${email.date}</div>
+            <div class="name">${email.from}</div>
+            <div class="message-content">
+               ${email.heading}
+            </div>
+        </div>
+    </div>
+    </li>`
+        $("#mails-inbox").append(emailElement);
+        console.log("hello")
+    });
 
+});
 
+function openMail(id) {
+    let email = emails.find(el => el.uid === id);
+    $('#inbox').hide();
+    $('#mail').show();
+    let emailElement = `
+    <h1 id="mailbox-header" >${email.heading}</h1>
+                    <div class="mailbox-list">
+                        <div>
+                            <div class="date">${email.date}</div>
+                            <div class="name">${email.from}</div>
+                        </div>
+                        <li class="message">
+                            ${email.message}
+                        </li>
+                    </div>`
+    $("#mail").append(emailElement);
+    console.log(email);
+}
