eca12e22b27190e11fc62990bba90c0fc2fe9ab2
Your Name <some@gmail.com>
2023-02-04 06:22:22 +0530

    Random spawn

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
-$('#window-profile').hide();
-$('#window-mail').hide();
-$('#window-help').hide();
+windowsList = ["window-mail", "window-help", "window-profile"]
+
+// Function to show window based on dev id
 function showWindow(windowId) {
     $('#' + windowId).show();
+}
+
+
+for (i = 0; i < windowsList.length; i++) {
+    currWindow = windowsList[i];
+    // Hide all windows on load
+    $("#" + currWindow).hide();
+    // Spawn all windows in random locations
+    var ht = Math.floor(Math.random() * (screen.height - 500)) + 100; // -500 so that it doesnt spawn half outside, and +100 as offset to see the logos
+    var w = Math.floor(Math.random() * (screen.width - 500)) + 100;
+    $("#" + currWindow).css({ top: ht, left: w, position: 'absolute' });
 }
