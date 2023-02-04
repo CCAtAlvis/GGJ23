6e18fe1e86db6723e0b572da239a03d909a3c569
Chinmay Chandak <chandakchinmay@gmail.com>
2023-02-05 03:49:39 +0530

    ai bot: added triggerAi() function

--- src/js/aiBot.js
+++ src/js/aiBot.js
@@ -3,7 +3,10 @@ const aiTexts = {
     2: 'some more text goes here'
 };
 
-function tirggerAi(index) {
-    
-}
+const aiBotBox = document.querySelector("#ai-container .box");
+const aiBottextContainer = document.querySelector("#ai-container .box__content");
 
+function tirggerAi(index) {
+    aiBotBox.style.display = 'block';
+    aiBottextContainer.textContent = aiTexts[index];
+}
