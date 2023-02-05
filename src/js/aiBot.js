57e7dbe4da33c4270a25216a957dc19693917f2c
Your Name <some@gmail.com>
2023-02-05 09:52:45 +0530

    Add close pointer to all windows

--- src/js/aiBot.js
+++ src/js/aiBot.js
@@ -7,14 +7,14 @@ const aiBotTexts = {
     6: 'Try finding the username of the owner',
     7: 'Congrats for completing the tutorial please wait while I find some data to start with.',
     8: 'Lets start by finding more info on Gautam Uchil use Profile for it',
-    9: 'Well lets try to connect to his system using his mail via ssh',
-    10: 'Looks like his pc is password protected lets brute force our way through it lets try someoff the most common passwords',
-    11: 'Explore through his pc and find some infomation which might be helpful to us',
+    9: 'Well lets try to connect to his system using his mail via SSH',
+    10: 'Looks like his pc is password protected lets brute force our way through it lets try some of the most common passwords',
+    11: 'Explore through his pc and find some information which might be helpful to us',
     12: 'Looks like we found out next target lets find more info about him',
-    13 : 'Lets do the same process for him again to connect to ssh',
+    13 : 'Lets do the same process for him again to connect to SSH',
     14 : 'In the past, adherence to important security protocols was not as stringent as it is now. It would be worthwhile to investigate the user\'s profile for additional information and insights.',
     15 : 'Time to explore',
-    16 : 'So these users have their default passowrd set as admin@123 , Instead of brute forcing and connecting to everyone maybe we can find info and connect to the most important person'
+    16 : 'These users might have never reset the passwords.'
 };
 
 const aiBotBox = document.querySelector("#ai-container .box");
